6a49bb53630917d5e932a79ac362b878c68e2b48
abstract <info@abstract-creative.com>
2020-03-21 21:39:26 -0500

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import SEO from '../components/seo';
 import Layout from '../containers/layout/layout'
 import Hero from '../containers/home-slider/hero'
 import Story from '../containers/home-slider/story'
-import ProjectSection from '../containers/home-slider/project'
+// import ProjectSection from '../containers/home-slider/project'
 import TestimonialSection from '../containers/home-slider/testimonial'
 import ClientSection from '../containers/home-slider/clients'
 import VideoSection from '../containers/home-slider/video'
@@ -14,7 +14,7 @@ export default () => (
         <SEO title="Home - Slider"/>
         <Hero/>
         <Story/>
-        <ProjectSection/>
+        {/* <ProjectSection/> */}
         <TestimonialSection/>
         <ClientSection/>
         <VideoSection/>
